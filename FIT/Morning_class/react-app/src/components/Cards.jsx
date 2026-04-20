41d73fcf578bcdf4078f5d1accc0712a2cf6deb2
Ahmed Ilyas <web.designer344@gmail.com>
2026-04-20 09:16:43 +0500

    changing

--- FIT/Morning_class/react-app/src/components/Cards.jsx
+++ FIT/Morning_class/react-app/src/components/Cards.jsx
@@ -4,7 +4,7 @@ function Cards(props){
     return(
         <div>
             <img/>
-            <div className="flex flex-col justify-center items-center m-20 bg-white text-start rounded-2xl p-5 w-80 h-70 gap-10 content-center shadow-lg">
+            <div className="flex flex-col justify-center items-center m-20 bg-white rounded-2xl p-5 w-80 h-70 gap-10 content-center shadow-lg">
                 <h1 className="text-xl font-bold py-3">{props.title}</h1>
                 <p className="text-sm text-gray-500 py-2">{props.desc}</p>
                 <button text-amber-400 py-4>Learn More</button>
