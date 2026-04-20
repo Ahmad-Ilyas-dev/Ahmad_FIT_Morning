088c9399a8f4df5c53050cc763752624c8b45e20
Ahmed Ilyas <web.designer344@gmail.com>
2026-04-20 11:23:50 +0500

    new method

--- FIT/Morning_class/react-app/src/App.jsx
+++ FIT/Morning_class/react-app/src/App.jsx
@@ -5,6 +5,14 @@ import Navbar from './components/Navbar'
 import Cards from './components/Cards'
 
 
+const courses = [
+  {title: 'Web Development', desc: 'Build Responsive WebSites', btn: 'Learn More'},
+  {title: 'SEO', desc: 'Rank Best WebSites', btn: 'Learn More'},
+  {title: 'Graphics', desc: 'Design Responsive WebSites', btn: 'Learn More'},
+  {title: 'AI', desc: 'Learn How to Train AI Model', btn: 'Learn More'},
+]
+
+
 function App() {
   
 
@@ -12,10 +20,12 @@ function App() {
     <>
     <div className='min-h-screen bg-gray-500 overflow-hidden'>
          <Navbar />
-         <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center p-5 m-3">
-         <Cards title='Web Development' desc='Build Responsive WebSites' />
-         <Cards title='SEO' desc='Rank Best WebSites' />
-         <Cards title='Graphics' desc='Design Responsive WebSites' />
+         <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center p-2 m-1">
+        
+          {courses.map((course) =>(
+            <Cards title={course.title} desc={course.desc} btn={course.btn} />
+          ))}
+        
          </div>
     </div>
        
--- FIT/Morning_class/react-app/src/components/Cards.jsx
+++ FIT/Morning_class/react-app/src/components/Cards.jsx
@@ -5,9 +5,9 @@ function Cards(props){
         <div>
             <img/>
             <div className="flex flex-col justify-center items-center m-20 bg-white rounded-2xl p-5 w-80 h-70 gap-10 content-center shadow-lg">
-                <h1 className="text-xl font-bold py-3">{props.title}</h1>
+                <h1 className="text-xl font-bold py-3 text-blue-700">{props.title}</h1>
                 <p className="text-sm text-gray-500 py-2">{props.desc}</p>
-                <button text-amber-400 py-4>Learn More</button>
+                <button className="text-amber-400 py-2 px-5 bg-blue-600 rounded-2xl">{props.btn}</button>
             </div>
         </div>
     )
